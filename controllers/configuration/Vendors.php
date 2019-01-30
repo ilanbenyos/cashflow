06fb4c87e5196ac6ab92bc510aee8884c985b289
skintivion <45840822+skintivion@users.noreply.github.com>
2019-01-30 17:31:08 +0530

    Update Vendors.php

--- controllers/configuration/Vendors.php
+++ controllers/configuration/Vendors.php
@@ -17,7 +17,7 @@ class Vendors extends CI_Controller {
 public function que()
 	{
 
-		 //echo $query1 = $this->db->query("ALTER TABLE `pspmaster` CHANGE `Commission` `Commission` DECIMAL(13,2) NOT NULL COMMENT 'transactions commission' ");
+		 echo $query1 = $this->db->query("ALTER TABLE `pspmaster` CHANGE `PspType` `TypeId` DECIMAL(11) NOT NULL");
 
 		
 		
