f776fc95a18d276f33753eac85d102d5bf00dbfa
Rasika <rasika@intivion.com>
2019-02-22 15:08:23 +0530

    links added in configuration

--- views/configuration/configuration.php
+++ views/configuration/configuration.php
@@ -20,6 +20,20 @@
           </div>
         </div>
       </div>
+      <div class="row">
+        <div class="main-icon-wrap clearfix">
+          <div class="col-md-3 col-sm-3 col-xs-6">
+            <div class="config-icon-box"> <a href="<?= base_url('expenses')?>"> <span class="icons"><img src="<?= base_url('assets/images/Expense.png')?>"/></span> <span class="medium-heading">Expenses</span> </a> </div>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-6">
+            <div class="config-icon-box"> <a href="<?= base_url('bank-transaction')?>""> <span class="icons"><img src="<?= base_url('assets/images/Bank-transaction.png')?>"/></span> <span class="medium-heading">Bank Transaction</span> </a> </div>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-6">
+            <div class="config-icon-box"> <a href="<?= base_url('Reports/psp_income')?>"> <span class="icons"><img src="<?= base_url('assets/images/reports.png')?>"/></span> <span class="medium-heading">Reports</span> </a> </div>
+          </div>
+          
+        </div>
+      </div>
     </div>
   </div>
 </div>
