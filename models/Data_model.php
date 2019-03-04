1a9f269cf405baf513054e67e5709f35d9517285
Rasika <rasika@intivion.com>
2019-03-04 13:50:21 +0530

    Update Data_model.php

--- models/Data_model.php
+++ models/Data_model.php
@@ -132,7 +132,6 @@ class Data_model extends CI_Model {
 		$this->db->select('distinct(Currency)');
 		$this->db->from('expenses');
 		$currency_array= $this->db->get()->result_array();
-		print_r($currency_array);exit();
 		 $this->db->select('ex.CatId,c.Category');
 		 foreach($currency_array as $currency){
 			$this->db->select('sum(if(ex.Currency ="'.$currency['Currency'].'",ex.ActualAmt,0))as '.$currency['Currency']);
@@ -148,7 +147,6 @@ class Data_model extends CI_Model {
 		$this->db->group_by('ex.CatId'); 
 	    $this->db->order_by('ex.CatId');
 	    return $this->db->get()->result_array();
-	    print_r($this->db->last_query());exit();
 	}
 
 	
