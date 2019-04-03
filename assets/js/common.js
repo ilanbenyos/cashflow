5d08397770c8660d771c346f0b7aacbd8fafce32
Aslam <aslam@intivion.com>
2019-04-03 13:08:55 +0530

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -56,7 +56,7 @@ var url = window.location;
 
 // for sidebar menu entirely
 jQuery(function ($) {
-    $(".components li a")
+    $(".sidebar-menu li a")
         .click(function(e) {
             var link = $(this);
             var item = link.parent("li");
