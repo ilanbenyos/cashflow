e0ecd8faa15b3b623c1ff7661051cf73f84fe1f9
Rasika <rasika@intivion.com>
2019-04-15 12:55:33 +0530

    Update footer.php

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -215,7 +215,7 @@ $('input.xyz').keyup(function(event) {
 
   $(this).val(function(index, value) {
     return value
-      //.replace(/\D/g, "")
+      .replace(/\D/g, "")
       //.regex(^(0|[1-9]\d*)$)
       //.replace(/([0-9])([0-9]{2})$/, '$1.$2')  
       .replace(/\B(?=(\d{3})+(?!\d)\.?)/g, ",")
