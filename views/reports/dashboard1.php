44eb4e5b5fa68e5fdebdb0cdcbcf70026c9f41d1
Rasika <rasika@intivion.com>
2019-05-21 11:03:34 +0530

    Update dashboard1.php

--- views/reports/dashboard1.php
+++ views/reports/dashboard1.php
@@ -168,7 +168,7 @@
               type: "angulargauge",
               renderAt: "chart-container",
               width: "100%",
-              height: "100%",
+              height: "90%",
               dataFormat: "json",
               dataSource
             }).render();
