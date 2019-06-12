98790a9a3882310133dff099a2f10ffa550f664c
Rasika <rasika@intivion.com>
2019-06-12 14:48:01 +0530

    Update updateCallCenterFund.php

--- views/updateCallCenterFund.php
+++ views/updateCallCenterFund.php
@@ -21,17 +21,15 @@
                 ?>
               <input type="hidden" name="editfund_token" value="<?php echo $token;?>">
               <input type="hidden" name="userid" value="<?php echo $_SESSION['userid'] ?>">
-			  <input type="hidden" name="callcenterid" value="<?php echo $callcenter_fund_details->id ?>">
-			   <input type="hidden" name="currency" id="currency" value="<?php echo $callcenter_fund_details->currency ?>">
-			   <input type="hidden" name="vendor_id" value="<?php echo $callcenter_fund_details->vendor_id ?>">
-              
+              <input type="hidden" name="callcenterid" value="<?php echo $callcenter_fund_details->id ?>">
+              <input type="hidden" name="currency" id="currency" value="<?php echo $callcenter_fund_details->currency ?>">
+              <input type="hidden" name="vendor_id" value="<?php echo $callcenter_fund_details->vendor_id ?>">
               <div class="row clearfix spacetop3x spacebottom2x">
                 <div class="clearfix row-flex">
-                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 common-border-box">
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="col-md-12 col-sm-12 col-xs-12 text-center">
                       <h4>General Information</h4>
                     </div>
-                    
                     <?php if ($_SESSION['user_role'] == "Admin"){?>
                     <div class="col-md-4 col-sm-12 col-xs-12">
                       <div class="form-group">
@@ -41,8 +39,7 @@
                         </div>
                       </div>
                     </div>
-					
-					<div class="col-md-4 col-sm-12 col-xs-12">
+                    <div class="col-md-4 col-sm-12 col-xs-12">
                       <div class="form-group">
                         <label class="col-md-5 col-sm-5 col-xs-12">Received Amount</label>
                         <div class="col-md-7 col-sm-7 col-xs-12">
@@ -50,18 +47,15 @@
                         </div>
                       </div>
                     </div>
-					
-					
-					<div class="col-md-4 col-sm-12 col-xs-12">
-                        <div class="form-group checkbox">
-                            <label>
-                              <input type="checkbox" name="received" id="received" <?= ( $callcenter_fund_details->status=='1'?  "checked" : "") ?>>
-                              <span class="cr"><i class="cr-icon fa fa-check"></i></span> <span class="acceptance">Is Amount Received</span> </label>
-                         
-                        </div>
-                      </div>
-                    <?php }elseif ($_SESSION['user_role'] == "Call Center User"){?>
                     <div class="col-md-4 col-sm-12 col-xs-12">
+                      <div class="form-group checkbox">
+                        <label>
+                          <input type="checkbox" name="received" id="received" <?= ( $callcenter_fund_details->status=='1'?  "checked" : "") ?>>
+                          <span class="cr"><i class="cr-icon fa fa-check"></i></span> <span class="acceptance">Is Amount Received</span> </label>
+                      </div>
+                    </div>
+                    <?php }elseif ($_SESSION['user_role'] == "Call Center User"){?>
+                    <div class="col-md-3 col-sm-12 col-xs-12">
                       <div class="form-group">
                         <label class="col-md-5 col-sm-5 col-xs-12">Amount Added</label>
                         <div class="col-md-7 col-sm-7 col-xs-12">
@@ -69,20 +63,17 @@
                         </div>
                       </div>
                     </div>
-					
-					<div class="col-md-4 col-sm-12 col-xs-12">
+                    <div class="col-md-3 col-sm-12 col-xs-12">
                       <div class="form-group">
                         <label class="col-md-5 col-sm-5 col-xs-12">Received Amount</label>
                         <div class="col-md-7 col-sm-7 col-xs-12">
                           <input type="text" class="form-control" name="receivedamount" id="receivedamount" <?php if($callcenter_fund_details->status == 1){ echo "readonly"; }?> value="<?php echo number_format($callcenter_fund_details->Amount_Received, 2, '.', ',') ?>" onkeypress="javascript:return isNumber(event)">
-                      
                         </div>
                       </div>
                     </div>
-					
-					<div class="col-md-4 col-sm-12 col-xs-12" id="currencyShow" style="display: none;">
+                    <div class="col-md-6 col-sm-12 col-xs-12" id="currencyShow" style="display: none;">
                       <div class="form-group">
-                <?php 
+                        <?php 
                 $this->db->select("CurId,CurName,CurSymbol,Active");
                 $this->db->from("currencymaster");
                 $this->db->where("CurId",$callcenter_fund_details->currency);
@@ -90,42 +81,35 @@
                 $curr=  $this->db->get ()->row ();
                 //print_r($curr->CurName);
                 ?>
-                  <div class="col-md-6 col-sm-4 col-xs-12">
-                             <select class="form-control" name="newCurr" id="newCurr" >
-                               <option value="">Select Currency</option>
+                        <div class="col-md-4 col-sm-4 col-xs-12">
+                          <select class="form-control" name="newCurr" id="newCurr" >
+                            <option value="">Select Currency</option>
                             <option value="1">EUR</option>
-                            <option value="<?php  echo $curr->CurId ?>"><?php echo $curr->CurName ?></option> 
+                            <option value="<?php  echo $curr->CurId ?>"><?php echo $curr->CurName ?></option>
                           </select>
                         </div>
-                
-                        
                         <div class="col-md-6 col-sm-4 col-xs-12" id="conversionCharges" style="display: none;">
-                             <input type="text" class="form-control" name="conversion_charges" id="conversion_charges" onkeypress="javascript:return isNumber(event)" placeholder="Conversion Charges">
-                        </div>
-                      </div>
-                    </div>
-                    
-					<div class="col-md-4 col-sm-12 col-xs-12">
-                        <div class="form-group checkbox">
+                          <input type="text" class="form-control" name="conversion_charges" id="conversion_charges" onkeypress="javascript:return isNumber(event)" placeholder="Conversion Charges">
+                        </div> 
+                        <div class="col-md-6 col-sm-4 col-xs-12">
+                          <div class="form-group checkbox">
                             <label>
                               <input type="checkbox" name="received" id="received" <?= ( $callcenter_fund_details->status=='1'?  "checked" : "") ?>>
                               <span class="cr"><i class="cr-icon fa fa-check"></i></span> <span class="acceptance">Is Amount Received</span> </label>
-                         
-                        </div>
+                          </div>
+                        </div> 
                       </div>
+                    </div>
                     <?php }?>
-                    
-                    
-                    
                   </div>
-                  <!--planned info starts -->
+                  <!--planned info starts --> 
                   
                   <!--planned info ends --> 
                   <!-- Actual info starts -->
                   
-					<div class="clearfix"></div>
+                  <div class="clearfix"></div>
                   <!--Actual info ends -->
-				  <?php if($callcenter_fund_details->status == 0){ ?>
+                  <?php if($callcenter_fund_details->status == 0){ ?>
                   <div class="col-xs-12 text-center spacetop2x">
                     <div class="page-loader" style="display:none;">
                       <div class="page-wrapper"> <span class="loader"><span class="loader-inner"></span></span> </div>
@@ -133,7 +117,7 @@
                     <button type="submit" id="editFund" class="btn-submit transitions">Submit</button>
                     <!-- <button type="reset" class="btn-reset transitions">Reset</button> --> 
                     <a href="<?= base_url('all-expenses');?>" class="btn-reset transitions" style="text-decoration: none;">Cancel</a> </div>
-					<?php } ?>
+                  <?php } ?>
                   <!-- </div> --> 
                 </div>
               </div>
